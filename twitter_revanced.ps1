ec69609c2ca4e68fe34de149800eb933c16fd360
Antonio Brugnolo <36473846+AntoSkate@users.noreply.github.com>
2022-07-27 17:56:23 +0200

    Add base_apk variable

--- twitter_revanced.ps1
+++ twitter_revanced.ps1
@@ -7,9 +7,12 @@ adb devices
 $adb = adb devices | Select-String -Pattern "\w+"
 $adb_device = $adb.Matches.Groups[1].Value
 
+# Get base apk name
+$base_apk = $args[0]
+
 # Install twitter
 
-java -jar revanced-cli-all.jar -a base.apk -c -d $adb_device -o twitter.apk -m integrations.apk -b revanced-patches.jar
+java -jar revanced-cli-all.jar -a $base_apk -c -d $adb_device -o twitter.apk -m integrations.apk -b revanced-patches.jar
 
 # Delete ReVanced files
 
--- youtube_revanced.ps1
+++ youtube_revanced.ps1
@@ -7,9 +7,12 @@ adb devices
 $adb = adb devices | Select-String -Pattern "\w+"
 $adb_device = $adb.Matches.Groups[1].Value
 
+# Get base apk name
+$base_apk = $args[0]
+
 # Install youtube (without root)
 
-java -jar revanced-cli-all.jar -a base.apk -c -d $adb_device -o youtube.apk -m integrations.apk -b revanced-patches.jar -e swipe-controls -e seekbar-tapping -e disable-create-button -e hide-autoplay-button -e old-quality-layout -e hide-shorts-button -e force-vp9-codec -e always-autorepeat -e enable-debugging -e hide-infocard-suggestions
+java -jar revanced-cli-all.jar -a $base_apk -c -d $adb_device -o youtube.apk -m integrations.apk -b revanced-patches.jar -e swipe-controls -e seekbar-tapping -e disable-create-button -e hide-autoplay-button -e old-quality-layout -e hide-shorts-button -e force-vp9-codec -e always-autorepeat -e enable-debugging -e hide-infocard-suggestions
 
 # Delete ReVanced files
 
